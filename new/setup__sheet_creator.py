1c5b916c857d4cc81a1a156d8bf64125772be3cd
Thomas BAGREL <tomsb07@gmail.com>
2018-03-28 18:43:23 +0200

    + news ohw ohw ohw

---
+++ new/setup__sheet_creator.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Setup tool for Sheet Creator X."""
+
+from cx_Freeze import setup, Executable
+import sys
+import os
+
+path = sys.path
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+bin_path_includes = []
+if sys.platform == "linux":
+    bin_path_includes += ["/usr/lib"]
+
+includes = []
+include_files = [
+    "resources",
+    "output",
+]
+packages = ["ui", "atexit"]
+excludes = ["tkinter"]
+
+options = {
+    "path": path,
+    "includes": includes,
+    "packages": packages,
+    "excludes": excludes,
+    "include_files": include_files,
+    "zip_include_packages": "*",
+    "zip_exclude_packages": [],
+    "bin_path_includes": bin_path_includes,
+}
+
+if sys.platform == "win32":
+    options["include_msvcr"] = True
+
+setup(
+    name="sheet_creator",
+    version="2.1.0",
+    description="Sheet Creator X",
+    executables=[
+        Executable("sheet_creator.py", base=base),
+    ],
+    options= {
+        "build_exe": options
+    },
+)
